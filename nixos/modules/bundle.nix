fa10e3b4abea0a0b9cae4b873d9ace6899cdd25d
LesVu <johndie005@gmail.com>
2025-01-28 20:27:29 +0800

    Add swapfile

--- nixos/modules/bundle.nix
+++ nixos/modules/bundle.nix
@@ -9,10 +9,10 @@
     ./nm.nix
     ./nvidia_graphics.nix
     ./sound.nix
+    ./swap.nix
     ./tailscale.nix
     ./user.nix
     ./virtualisation.nix
     ./xserver.nix
-    ./zram.nix
   ];
 }
--- nixos/modules/swap.nix
+++ nixos/modules/swap.nix
@@ -5,4 +5,8 @@
     memoryPercent = 50;
     priority = 999;
   };
+  swapDevices = [{
+    device = "/var/lib/swapfile";
+    size = 16 * 1024;
+  }];
 }
