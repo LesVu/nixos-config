93b681a8b644af4e4f9d243994aa2fb18543a97c
LesVu <johndie005@gmail.com>
2025-03-15 20:48:35 +0800

    Update

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     lix = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.1-2.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
