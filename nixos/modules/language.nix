ae8de2fd7de729c7fd067c80e5951f58a4e6d2ab
LesVu <placeholder>
2025-08-15 00:37:30 +0800

    Up

--- nixos/modules/language.nix
+++ nixos/modules/language.nix
@@ -19,7 +19,11 @@
       type = "fcitx5";
       fcitx5 = {
         waylandFrontend = true;
-        addons = with pkgs; [ fcitx5-chinese-addons ];
+        addons = with pkgs; [
+          fcitx5-chinese-addons
+          fcitx5-pinyin-zhwiki
+          fcitx5-breeze
+        ];
       };
     };
   };
